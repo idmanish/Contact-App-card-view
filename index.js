add3225d4911f699f931871f35fc17847873f478
manish <idmanish@gmail.com>
2015-07-06 23:20:53 +0530

    make it better

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(index.html);
+  res.sendfile('index.html');
 });
 
 app.listen(app.get('port'), function() {
